44ebcef3f767d10fa57e3a634caf4633ac81916f
Jakob Reumann <32244260+nogxx@users.noreply.github.com>
2018-11-14 21:38:07 +0100

    updated intent returns

--- lambda_function.py
+++ lambda_function.py
@@ -30,7 +30,8 @@ def get_exapmple_response(intent, session):
         should_end_session = False
     
     return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+        speech_output, reprompt_text, should_end_session, card_title,
+        output_type="SSML"))
 
 def get_welcome_response():
     """ Example of how a welcome response would look like
@@ -45,7 +46,8 @@ def get_welcome_response():
     should_end_session = False
     
     return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+        speech_output, reprompt_text, should_end_session, card_title,
+        output_type="SSML"))
 
 def handle_session_end_request():
     card_title = "Session Ended"
@@ -53,8 +55,10 @@ def handle_session_end_request():
                     A 5-star rating would be very much appreciated."
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
-    return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
+    
+    return build_response(session_attributes, build_speechlet_response(
+        speech_output, reprompt_text, should_end_session, card_title,
+        output_type="SSML"))
 
 # --------------- Events ------------------
 
